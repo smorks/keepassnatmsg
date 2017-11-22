997d5eee5fb5c6b7a98c59029eed8604241274e0
Andy Brandt <andy@brandt.tech>
2017-11-21 23:52:00 -0600

    update Assembly info

--- KeePassHttp/Properties/AssemblyInfo.cs
+++ KeePassHttp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("KeePassHttp")]
-[assembly: AssemblyDescription("A plugin to expose a secure HTTP interface to your KeePass database")]
+[assembly: AssemblyDescription("A plugin to expose a secure interface to your KeePass database")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Perry Nguyen, Lukas Schulze")]
+[assembly: AssemblyCompany("Andy Brandt")]
 [assembly: AssemblyProduct("KeePass Plugin")]
-[assembly: AssemblyCopyright("Copyright © Perry Nguyen, Lukas Schulze 2013")]
+[assembly: AssemblyCopyright("Copyright © Andy Brandt 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-//[assembly: AssemblyFileVersion("2.0.0.0")]
